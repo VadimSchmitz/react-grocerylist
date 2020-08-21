37cd2e12f260bffb5986935702d0af3c3a2d4067
VadimSchmitz <di3mpi3@gmail.com>
2020-08-21 15:56:16 +0200

    passes data to child

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Grocerylist from './Components/Grocerylist'
+import groceryData from './groceryData'
 
 function App() {
   return (
     <div className="App">
-
+      <Grocerylist initialItems={groceryData}/>
     </div>
   );
 }
