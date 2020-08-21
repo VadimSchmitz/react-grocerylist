9dc8ffd11aafdd5dd96fc13f5fd33209d662c553
VadimSchmitz <di3mpi3@gmail.com>
2020-08-21 15:56:23 +0200

    data to play with

---
+++ src/groceryData.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: "1",
+        name: "Pencil",
+        price: 1,
+        qty: 2,
+    },
+    {
+        id: "2",
+        name: "gum",
+        price: 1.5,
+        qty: 3,
+    },
+    {
+        id: "3",
+        name: "paper",
+        price: 2,
+        qty: 1,
+    },
+    {
+        id: "4",
+        name: "Coffee",
+        price: 2,
+        qty: 6,
+    },
+    {
+        id: "5",
+        name: "Snacks",
+        price: 4,
+        qty: 1,
+    },
+]
+
+export default products
